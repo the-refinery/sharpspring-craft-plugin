e48942c79a9aa366f99738afd3f91bbedee9498f
Brady Bonnette <brady.bonnette@the-refinery.io>
2019-11-21 22:49:40 -0500

    Adds native form support
    (cherry picked from commit 5051572c40b12074f4210179fe4d0c9875b023e1)

---
+++ responses/NativeFormResponse.php
@@ -0,0 +1,47 @@
+<?php
+namespace Craft\SharpSpringIntegration\Responses;
+
+class NativeFormResponse
+{
+  public $data;
+
+  public function __construct($data) {
+    $this->data = $data;
+  }
+
+  public function getError() {
+    return $this->getErrorMessage();
+  }
+
+  public function hasErrors() {
+    $test = "heh";
+
+    $matchResult = preg_match('/_ss_noform.success = ([^;]*);/', $this->data, $matches);
+
+    if($matchResult === 0 || $matchResult === false) {
+      SharpspringIntegrationPlugin::log("There was an issue determining errors from sharpspring native form post:\n\n".$this->data, LogLevel::Warning, true);
+      return false;
+    }
+
+    // "_ss_noform.success = false;" means failure
+    // "_ss_noform.success = true;" means success
+
+    return $matches[1] == "false";
+  }
+
+  public function getErrorMessage() {
+    if($this->hasErrors()) {
+      $matchResult = preg_match('/_ss_noform.error = ([^;]*);/', $this->data, $matches);
+
+      if($matchResult === 0 || $matchResult === false) {
+        SharpspringIntegrationPlugin::log("There was an issue extracting error message from native form post:\n\n".$this->data, LogLevel::Warning, true);
+        return null;
+      }
+
+      return $matches[1];
+    }
+
+    return null;
+  }
+}
+
